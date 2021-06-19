eeb39289e157919bbbceae03ed4745113ee7f976
liyu022 <549277739@qq.com>
2021-06-19 14:09:04 +0800

    iii

--- README.md
+++ README.md
@@ -1,1 +1,126 @@
-# vite-plugin-plus-cesium
+# ⚡ vite-plugin-cesium
+
+[![npm](https://img.shields.io/npm/v/vite-plugin-cesium.svg)](https://www.npmjs.com/package/vite-plugin-cesium)
+
+Easily set up a [`Cesium`] project in [`Vite`].
+
+[`cesium`]: https://github.com/CesiumGS/cesium
+[`vite`]: https://github.com/vitejs/vite
+
+**update：** if you just wanna a scaffolding by using this plugin, try a simply command `yarn create cesium`, click [create-cesium](https://www.npmjs.com/package/create-cesium) for more info.
+
+Chinese tutorial: [中文教程](https://segmentfault.com/a/1190000039344137)
+
+## Install
+
+```bash
+npm i cesium vite-plugin-cesium vite -D
+# yarn add cesium vite-plugin-cesium vite -D
+```
+
+## Usage
+
+add this plugin to `vite.config.js`
+
+```js
+import { defineConfig } from 'vite';
+import cesium from 'vite-plugin-cesium';
+export default defineConfig({
+  plugins: [cesium()]
+});
+```
+
+add dev command to `package.json`
+
+```json
+"scripts": {
+  "dev": "vite",
+  "build": "vite build"
+}
+```
+
+run:
+
+`yarn dev`
+
+## Options
+
+**rebuildCesium**
+
+- **Type :** `boolean`
+- **Default :** `false`
+
+Default copy min cesium file to dist. if `true` will rebuild cesium from source.
+
+```js
+import { defineConfig } from 'vite';
+import cesium from 'vite-plugin-cesium';
+export default defineConfig({
+  plugins: [
+    cesium({
+      rebuildCesium: true
+    })
+  ]
+});
+```
+
+## Demo
+
+`src/index.js`
+
+```js
+import { Viewer } from 'cesium';
+import './css/main.css';
+
+const viewer = new Viewer('cesiumContainer');
+```
+
+`index.html`
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>cesium-vite</title>
+    <script type="module" src="/src/index.js"></script>
+  </head>
+
+  <body>
+    <div id="cesiumContainer"></div>
+  </body>
+</html>
+```
+
+`src/css/main.css`
+
+```css
+html,
+body,
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+```
+
+Add `dev` and `build` commands to `package.json`
+
+```
+"scripts": {
+    "dev": "vite",
+    "build": "vite build"
+},
+```
+
+Run `yarn dev`
+
+For full demo project please check [./demo](https://github.com/nshen/vite-plugin-cesium/tree/main/demo) folder.
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,117 @@
+import path from 'path';
+import { Plugin, UserConfig, HtmlTagDescriptor } from 'vite';
+import fs from 'fs-extra';
+import externalGlobals from 'rollup-plugin-external-globals';
+import serveStatic from 'serve-static';
+
+interface VitePluginCesiumOptions {
+  rebuildCesium?: boolean;
+  devMinifyCesium?: boolean;
+}
+
+function vitePluginCesium(
+  options: VitePluginCesiumOptions = {
+    rebuildCesium: false,
+    devMinifyCesium: false
+  }
+): Plugin {
+  const { rebuildCesium, devMinifyCesium } = options;
+
+  const cesiumBuildRootPath = 'node_modules/cesium/Build';
+  const cesiumBuildPath = 'node_modules/cesium/Build/Cesium/';
+  const CESIUM_BASE_URL = 'cesium';
+
+  let publicPath = 'public';
+  let outDir = 'dist';
+  let base: string;
+  let isBuild: boolean = false;
+
+  return {
+    name: 'vite-plugin-cesium',
+
+    config(_, { command }) {
+      const userConfig: UserConfig = {
+        build: {
+          assetsInlineLimit: 0,
+          chunkSizeWarningLimit: 4000
+        },
+        define: {
+          CESIUM_BASE_URL: JSON.stringify(CESIUM_BASE_URL)
+        }
+      };
+      if (command === 'build' && !rebuildCesium) {
+        userConfig.build!.rollupOptions = {
+          external: ['cesium'],
+          plugins: [externalGlobals({ cesium: 'Cesium' })]
+        };
+      }
+
+      return userConfig;
+    },
+
+    configResolved(resolvedConfig) {
+      publicPath = resolvedConfig.publicDir;
+      outDir = resolvedConfig.build.outDir;
+      base = resolvedConfig.base;
+      isBuild =
+        resolvedConfig.isProduction || resolvedConfig.command === 'build';
+    },
+
+    configureServer({ middlewares }) {
+      const cesiumPath = path.join(
+        cesiumBuildRootPath,
+        devMinifyCesium ? 'Cesium' : 'CesiumUnminified'
+      );
+      middlewares.use(CESIUM_BASE_URL, serveStatic(cesiumPath));
+    },
+
+    async closeBundle() {
+      if (isBuild) {
+        try {
+          await fs.copy(
+            path.join(cesiumBuildPath, 'Assets'),
+            path.join(outDir, 'cesium/Assets')
+          );
+          await fs.copy(
+            path.join(cesiumBuildPath, 'ThirdParty'),
+            path.join(outDir, 'cesium/ThirdParty')
+          );
+          await fs.copy(
+            path.join(cesiumBuildPath, 'Workers'),
+            path.join(outDir, 'cesium/Workers')
+          );
+          await fs.copy(
+            path.join(cesiumBuildPath, 'Widgets'),
+            path.join(outDir, 'cesium/Widgets')
+          );
+          if (!rebuildCesium) {
+            await fs.copy(
+              path.join(cesiumBuildPath, 'Cesium.js'),
+              path.join(outDir, 'cesium/Cesium.js')
+            );
+          }
+        } catch (err) {
+          console.error('copy failed', err);
+        }
+      }
+    },
+
+    transformIndexHtml() {
+      const tags: HtmlTagDescriptor[] = [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'stylesheet',
+            href: base + 'cesium/Widgets/widgets.css'
+          }
+        }
+      ];
+      if (isBuild && !rebuildCesium) {
+        tags.push({ tag: 'script', attrs: { src: base + 'cesium/Cesium.js' } });
+      }
+      return tags;
+    }
+  };
+}
+
+export default vitePluginCesium;
